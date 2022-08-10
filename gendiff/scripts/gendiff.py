06119f01661f98c5023a1cf61025f17060a292fe
SaveDim <jieto35@yandex.ru>
2022-08-10 09:08:49 +0300

    Delete unnecessary variable

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -11,7 +11,7 @@ def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file', metavar='first_file', type=str)
     parser.add_argument('second_file', metavar='second_file', type=str)
-    args = parser.parse_args()
+    parser.parse_args()
 
 if __name__ == '__main__':
     main()
