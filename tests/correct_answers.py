268a3d8ac3854bf4335ff8e8819f055b9476ba5e
SaveDim <jieto35@yandex.ru>
2022-08-19 11:55:20 +0300

    Add tests

---
+++ tests/correct_answers.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Expected results."""
+
+CORRECT_FLAT_RESULT = '''{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}'''
---
+++ tests/test_diff_json.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""JSON files test diff."""
+
+from gendiff.generate_diff import generate_diff
+import tests.correct_answers as expected
+
+
+def test1_flat_json():
+    correct = generate_diff('./fixtures/file1_flat.json',
+                           './fixtures/file2_flat.json',
+                           )
+    assert correct == expected.CORRECT_FLAT_RESULT
