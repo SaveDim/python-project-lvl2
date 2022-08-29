8bb77ef504973cf1f5b37b790e19f230d1a3f8d1
SaveDim <jieto35@yandex.ru>
2022-08-29 11:22:52 +0300

    Change path to json file

--- tests/test_diff_json.py
+++ tests/test_diff_json.py
@@ -7,7 +7,7 @@ import tests.correct_answers as expected
 
 
 def test1_flat_json():
-    correct = generate_diff('./fixtures/file1_flat.json',
-                           './fixtures/file2_flat.json',
+    correct = generate_diff('./tests/fixtures/file1_flat.json',
+                           './tests/fixtures/file2_flat.json',
                            )
     assert correct == expected.CORRECT_FLAT_RESULT
