09e8d85f01c361a79909c41782d50bbb42cf3ad3
SaveDim <jieto35@yandex.ru>
2022-09-01 13:50:30 +0300

    Add exceptions to setup.cfg

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -4,6 +4,7 @@ import json
 
 
 def open_file(file_path1, file_path2):
+    """Return two dictionaries."""
     with open(file_path1, 'r') as file1:
         file1 = json.load(file1)
     with open(file_path2, 'r') as file2:
@@ -24,7 +25,7 @@ def generate_diff(file_path1, file_path2):
             diff += f'    {key}: {file1_value}\n'
         elif key in keys_file2 and file1[key] != file2[key]:
             diff += f'  - {key}: {file1_value}\n' \
-                      f'  + {key}: {str(file2[key]).lower()}\n'
+                    f'  + {key}: {str(file2[key]).lower()}\n'
     for key in keys_file2:
         if key not in keys_file1:
             diff += f'  + {key}: {str(file2[key]).lower()}\n'
