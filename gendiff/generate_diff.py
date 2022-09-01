d4525d0390abe9950e16a23a3f8686efdfe73739
SaveDim <jieto35@yandex.ru>
2022-09-01 09:15:27 +0300

    Fix bugs

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -13,14 +13,14 @@ def generate_diff(file_path1, file_path2):
     keys_file1, keys_file2 = list(file1.keys()), list(file2.keys())
     for key in sorted(keys_file1):
         if key not in keys_file2:
-            result += f' - {str(key)}: {str(file1[key]).lower()}\n'
+            result += f'  - {key}: {str(file1[key]).lower()}\n'
         elif key in keys_file2 and file1[key] == file2[key]:
-            result += f'    {str(key)}: {str(file1[key]).lower()}\n'
+            result += f'    {key}: {str(file1[key]).lower()}\n'
         elif key in keys_file2 and file1[key] != file2[key]:
-            result += f'  - {str(key)}: {str(file1[key]).lower()}\n'
-            result += f'  + {str(key)}: {str(file2[key]).lower()}\n'
+            result += f'  - {key}: {str(file1[key]).lower()}\n' \
+                      f'  + {key}: {str(file2[key]).lower()}\n'
     for key in keys_file2:
         if key not in keys_file1:
-            result += f'  + {str(key)}: {str(file2[key]).lower()}\n'
+            result += f'  + {key}: {str(file2[key]).lower()}\n'
     result += '}'
     return result
