8d875e48fe30c5ba02ad4c4c0dd511e1bcd4546b
SaveDim <jieto35@yandex.ru>
2022-09-01 08:37:21 +0300

    Make func easier

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -5,25 +5,22 @@ import json
 
 def generate_diff(file_path1, file_path2):
     """Return difference between file_path1 and file_path2."""
-
-    # Дмитрий получи из этой части функцию, которая возвращает списки ключей для сравнения!!!
-
     with open(file_path1, 'r') as file1:
         file1 = json.load(file1)
     with open(file_path2, 'r') as file2:
         file2 = json.load(file2)
     result = '{\n'
     keys_file1, keys_file2 = list(file1.keys()), list(file2.keys())
-    for i in sorted(keys_file1):
-        if i not in keys_file2:
-            result += '  - ' + str(i) + ': ' + str(file1[i]).lower() + '\n'
-        elif i in keys_file2 and file1[i] == file2[i]:
-            result += '    ' + str(i) + ': ' + str(file1[i]).lower() + '\n'
-        elif i in keys_file2 and file1[i] != file2[i]:
-            result += '  - ' + str(i) + ': ' + str(file1[i]).lower() + '\n'
-            result += '  + ' + str(i) + ': ' + str(file2[i]).lower() + '\n'
-    for i in keys_file2:
-        if i not in keys_file1:
-            result += '  + ' + str(i) + ': ' + str(file2[i]).lower() + '\n'
+    for key in sorted(keys_file1):
+        if key not in keys_file2:
+            result += f' - {str(key)}: {str(file1[key]).lower()}\n'
+        elif key in keys_file2 and file1[key] == file2[key]:
+            result += f'    {str(key)}: {str(file1[key]).lower()}\n'
+        elif key in keys_file2 and file1[key] != file2[key]:
+            result += f'  - {str(key)}: {str(file1[key]).lower()}\n'
+            result += f'  + {str(key)}: {str(file2[key]).lower()}\n'
+    for key in keys_file2:
+        if key not in keys_file1:
+            result += f'  + {str(key)}: {str(file2[key]).lower()}\n'
     result += '}'
     return result
