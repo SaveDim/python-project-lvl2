5db28835fd83d4df14db5e8cf119cf8fb11d4f85
SaveDim <jieto35@yandex.ru>
2022-09-02 13:07:02 +0300

    Make code better

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -14,11 +14,7 @@ def main():
         description='Compares two configuration files and shows a difference.',
     )
     parser.add_argument('first_file', metavar='first_file', type=str)
-    parser.add_argument(
-        'second_file',
-        metavar='second_file',
-        type=str,
-    )
+    parser.add_argument('second_file', metavar='second_file', type=str)
     parser.add_argument(
         '-f',
         '--format',
