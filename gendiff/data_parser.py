4d9effc0aa6eb004b336ecb18cc951b0505dbf56
SaveDim <jieto35@yandex.ru>
2022-09-06 09:50:47 +0300

    Fix linter bugs

--- gendiff/data_parser.py
+++ gendiff/data_parser.py
@@ -6,7 +6,7 @@ import yaml
 
 def get_file_data(path_to_file):
     if path_to_file.endswith('.json'):
-        file_data = json.load(open(path_to_file))
+        file_data = json.load(open(path_to_file, 'r'))
     elif path_to_file.endswith('.yaml') or path_to_file.endswith('.yml'):
-        file_data = yaml.safe_load(open(path_to_file))
+        file_data = yaml.safe_load(open(path_to_file, 'r'))
     return file_data
