27b97dba377c0e04218f3e6cda577ba08e62a750
SaveDim <jieto35@yandex.ru>
2022-09-13 14:17:11 +0300

    Fix import order

--- gendiff/data_parser.py
+++ gendiff/data_parser.py
@@ -1,13 +1,31 @@
 """Parse data within .json, .yaml and .yml files."""
 
 import json
+from pathlib import Path
 
 import yaml
 
 
 def get_file_data(path_to_file):
-    if path_to_file.endswith('.json'):
-        file_data = json.load(open(path_to_file, 'r'))
-    elif path_to_file.endswith('.yaml') or path_to_file.endswith('.yml'):
-        file_data = yaml.safe_load(open(path_to_file, 'r'))
+    format = get_extention(path_to_file)
+    file_data = load_file_data(open_file(path_to_file), format)
     return file_data
+
+
+def get_extention(path_to_file):
+    extention = Path(path_to_file).suffix
+    if extention.lower() == '.json':
+        return 'json'
+    elif extention == '.yaml' or extention == '.yml':
+        return 'yaml'
+
+
+def open_file(fielname):
+    return open(fielname)
+
+
+def load_file_data(filename, format):
+    if format == 'json':
+        return json.load(filename)
+    elif format == 'yaml':
+        return yaml.safe_load(filename)
