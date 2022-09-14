a1258f07f7d8a4b90738d78780681f98f3135c4b
SaveDim <jieto35@yandex.ru>
2022-09-14 13:27:22 +0300

    Add plain logic

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -13,18 +13,18 @@ def main():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.',
     )
-    parser.add_argument('first_file', metavar='first_file', type=str)
-    parser.add_argument('second_file', metavar='second_file', type=str)
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
     parser.add_argument(
         '-f',
         '--format',
-        action='store',
+        default='stylish',
         help='set format of output',
     )
     parser.parse_args()
     args = parser.parse_args()
 
-    print(generate_diff(args.first_file, args.second_file))
+    print(generate_diff(args.first_file, args.second_file, args.format))
 
 
 if __name__ == '__main__':
