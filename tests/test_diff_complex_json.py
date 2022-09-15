9c4e8608f36bcdea3523a72a7964a4a09e4eefe1
SaveDim <jieto35@yandex.ru>
2022-09-15 13:19:47 +0300

    Add answers to files, fix test

--- tests/test_diff_complex_json.py
+++ tests/test_diff_complex_json.py
@@ -4,11 +4,12 @@
 """JSON files test diff."""
 
 from gendiff.generate_diff import generate_diff
-import tests.fixtures.correct_answers as expected
 
 
 def test1_complex_json():
     correct = generate_diff('./tests/fixtures/file1_complex.json',
                             './tests/fixtures/file2_complex.json',
                             )
-    assert correct == expected.CORRECT_COMPLEX_RESULT
+    expected = open('./tests/fixtures'
+                    '/correct_complex_result.txt', 'r').read()
+    assert correct == expected
--- tests/test_diff_json.py
+++ tests/test_diff_json.py
@@ -4,11 +4,12 @@
 """JSON files test diff."""
 
 from gendiff.generate_diff import generate_diff
-import tests.fixtures.correct_answers as expected
 
 
 def test1_flat_json():
     correct = generate_diff('./tests/fixtures/file1_flat.json',
                             './tests/fixtures/file2_flat.json',
                             )
-    assert correct == expected.CORRECT_FLAT_RESULT
+    expected = open('./tests/fixtures'
+                    '/correct_flat_result.txt', 'r').read()
+    assert correct == expected
