35ec206db45fbaf2475df4472ec2956194f4480a
DS <jieto35@yandex.ru>
2022-12-21 15:02:42 +0300

    Change function name

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -3,12 +3,12 @@
 from gendiff.formatters.getting_format import choose_format
 
 from .data_parser import get_file_data
-from .gen_dict_diff import dict_differencer
+from .gen_dict_diff import build_diff
 
 
 def generate_diff(file_path1, file_path2, format='stylish'):
     """Return difference between file_path1 and file_path2."""
     file1 = get_file_data(file_path1)
     file2 = get_file_data(file_path2)
-    diff = dict_differencer(file1, file2)
+    diff = build_diff(file1, file2)
     return choose_format(diff, format)
